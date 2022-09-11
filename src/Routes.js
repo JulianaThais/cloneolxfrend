a28cdcdd21a53ba53b38a69d85ae8f3291a5138a
JulianaThais <julianaveloso611@gmail.com>
2022-09-11 14:00:28 -0300

    Menu basico - Teste login (Cookie) - 484

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home'
-import About from './pages/About'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
-    return (
-        <Routes>
-            <Route exact path="/" element={<Home/>}/>
-            <Route exact path="/about" element={<About/>}/>
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/about" element={<About />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
--- src/components/partials/Header/styled.js
+++ src/components/partials/Header/styled.js
@@ -31,4 +31,41 @@ export const HeaderArea = styled.div`
         .logo-2 {color:#0F0}
         .logo-3 {color:#00F}
     }
+    nav{
+        padding-top: 10px;
+        padding-bottom: 10px;
+
+        ul,li {
+            margin: 0;
+            padding: 0;
+            list-style: none;
+        }
+        ul {
+            display: flex;
+            alig-items: center;
+            height:40px;
+        }
+
+        li{
+            margin-left:20px;
+            margin-right:20px;
+        }
+          a{
+            color: #000;
+            font-size: 14px;
+
+            &:hover{
+                color:#999;
+            }
+            &.button{
+                background-color: #ff8100;
+                border-radius: 4px;
+                color:#fff;
+                padding:5px 10px;
+            }
+            &.button:hover {
+                background-color:#e57706;
+            }
+          }
+    }
     `
